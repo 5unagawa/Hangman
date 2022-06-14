cc49ae047edac73cc6c207bd8ff123ffec5029d2
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-06-14 09:39:45 +0900

    Create dictionary.js

---
+++ modules/dictionary.js
@@ -0,0 +1,42 @@
+var firstGradeDictionary = [
+    "abroad", "absent", "across", "active", "adult", "ago", "air", "all", "also", "amazing", 
+    "android", "angel", "another", "answer", "ant", "any", "anything", "anywhere", "apron",
+    "athlete", "attack", "aunt", "aurora", "avocado", "awesome", "back", "bad", "bang", "barbecue",
+    "basilisk", "bathroom", "beast", "beat", "bedroom", "beefsteak", "before", "beyond", "bicycle",
+    "bike",  "bird", "bitter", "both", "bought", "break", "bridge", "broke", "burger", "bye",
+  "April", "China", "Egypt", "India", "Italy", "Japan", "Korea", "March", "Spain", "about",
+    "again", "apple", "baker", "beach", "black", "brown", "chair", "class", "clean", "color",
+    "dance", "drama", "dream", "drink", "eight", "enjoy", "field", "fifth", "fifty", "first",
+    "forty", "fruit", "glove","grape", "green", "happy", "hello", "horse", "house", "juice",
+    "leave", "lemon", "lunch", "melon", "mouth", "music", "never", "ninth", "nurse", "panda",
+    "peach", "piano", "pilot", "pizza", "place", "queen", "rainy", "river", "rugby", "ruler",
+    "salad", "salty", "seven", "sheep", "short", "sixth", "sixty", "small", "snake", "snowy",
+    "sorry", "speak", "sport", "stand", "steak", "study", "sunny", "table", "tenth", "think",
+    "third", "three", "tiger", "tired", "under", "visit", "watch", "where", "white", "world"
+]
+
+var secondGradeDictionary = [
+    "above", "actor", "agree", "alone", "along", "among", "award", "bacon", "blame", "bored",
+    "bring", "build", "built", "carry", "cheap", "chess", "cloth", "coast", "could", "cover",
+    "dirty", "eaten", "email", "empty", "enter", "fight", "flour", "goods", "guide", "heard",
+    "heart", "heavy", "hurry", "inner", "knife", "ladle", "later", "limit", "lives", "local",
+    "lodge", "lotus", "meter", "minus", "model", "money", "noise", "novel", "onion", "order",
+    "paint", "paper", "peace", "penny", "piece", "plant", "prize", "ready", "rifle", "sauce",
+    "scale", "score", "shelf", "shout", "since", "slept", "smoke", "solve", "space", "spite",
+    "spoke", "steal", "stole", "story", "stove", "sugar", "sweat", "taken", "taste", "teeth",
+    "thick", "tooth", "tower", "treat", "trick", "truck", "truly", "twice", "voice", "while",
+    "windy", "worry", "wrong", "wrote", "yummy"
+]
+
+var thirdGradeDictionary = [
+    "allow", "angry", "ankle", "arena", "began", "begin", "bench", "bleed", "brain", "bread",
+    "brush", "budge", "burst", "canoe", "cheek", "chest", "chose", "comfy", "cross", "crush",
+    "daily", "debut", "dress", "drill", "dryer", "enemy", "fence", "fever", "final", "float",
+    "force", "given", "group", "grown", "habit", "human", "image", "laugh", "least", "level",
+    "lower", "march", "match", "meant", "might", "north", "ocean", "patch", "pause", "pinky",
+    "power", "price", "radio", "raise", "relay", "scene", "scowl", "serve", "shake", "shape",
+    "shawl", "sheet", "shirt", "silly", "skirt", "smell", "solid", "south", "spell", "spend",
+    "spent", "spice", "stack", "stage", "stain", "stern", "stood", "super", "thigh", "thumb",
+    "tight", "title", "toast", "trash", "trunk", "trust", "whole", "worse", "wrist"
+]
+export {firstGradeDictionary, secondGradeDictionary, thirdGradeDictionary};
